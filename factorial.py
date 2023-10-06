14b788f8680661606c6fa718b336d365b23464f8
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-06 12:18:16 +0530

    Update factorial.py
    changes

--- factorial.py
+++ factorial.py
@@ -1,11 +1,12 @@
+factorial(n);
 def factorial(n):
 
-    res = 1
+    RES = 1
 
     for i in range(2, n+1):
-        res *= i
-    return res
+        RES *= i
+    return RES
  # Driver Code
-num = 5
-print("Factorial of", num, "is",
-factorial(num))
+NUM = 5
+print("Factorial of", NUM, "is",
+factorial(NUM))
