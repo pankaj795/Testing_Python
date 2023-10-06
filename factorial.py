2fab3bbc85c1f0ab7a67e049d93b71f7462f36dc
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-06 15:50:06 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,13 +1,22 @@
-def factorial(n):
-   
-    if n < 0:
-        raise ValueError("Input must be a non-negative integer.")
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
+def sum_of_three_numbers(a, b, c):
+    """
+    Calculate the sum of three numbers.
+
+    Args:
+        a (int): The first number.
+        b (int): The second number.
+        c (int): The third number.
+
+    Returns:
+        int: The sum of the three numbers.
+    """
+    return a + b + c
 
 if __name__ == "__main__":
-    n = int(input("Enter a non-negative integer: "))
-    result = factorial(n)
-    printf(f"The factorial of the file emlmwelwenflenferkjnfekjfneknef nerke fnek fef emf,e fme {n} is: {result}")
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    num3 = int(input("Enter the third number: "))
+
+    result = sum_of_three_numbers(num1, num2, num3)
+
+    print(f"The sum of {num1}, {num2}, and {num3} is: {result}")
