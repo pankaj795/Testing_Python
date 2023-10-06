4493a80a51b31b71396d1622af36aa6342c50f0f
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-06 15:14:45 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,13 +1,13 @@
-def factorial(p):
+def factorial(n):
    
     if n1 < 0:
         raise ValueError("Input must be a non-negative integer.")
-    if p == 0:
+    if n == 0:
         return 1
     else:
-        return p * factorial(n - 1)
+        return n * factorial(n - 1)
 
 if __name__ == "__main__":
-    p = int(input("Enter a non-negative integer: "))
-    result = factorial(p)
-    print(f"The factorial of the file emlmwelwenflenferkjnfekjfneknef nerke fnek fef emf,e fme {p} is: {result}")
+    n = int(input("Enter a non-negative integer: "))
+    result = factorial(n)
+    print(f"The factorial of the file emlmwelwenflenferkjnfekjfneknef nerke fnek fef emf,e fme {n} is: {result}")
