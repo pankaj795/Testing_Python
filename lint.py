a53389e06518472a2eba32464d51a7c1ece2144f
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-06 15:19:58 +0530

    Update lint.py

--- lint.py
+++ lint.py
@@ -1,22 +1,28 @@
 # lint.py
 import pylint.lint
+import subprocess
 
-def lint_python_code(file_path):
+def lint_and_fix_python_code(file_path):
     # Customize pylint options as needed
     pylint_opts = [
         '--rcfile=pylintrc',  # Path to your pylint configuration file
+        '--load-plugins', 'pylint.extensions.mccabe',
         file_path,
     ]
-    
+
     # Run pylint and return its exit code
     exit_code = pylint.lint.Run(pylint_opts).linter.msg_status
+
+    # Automatically fix linting issues using autopep8
+    subprocess.run(["autopep8", "--in-place", "--aggressive", file_path])
+
     return exit_code
 
 if __name__ == "__main__":
     file_path = "factorial.py"  # Replace with your Python file path
     threshold = 7  # Set your linting threshold here
 
-    exit_code = lint_python_code(file_path)
+    exit_code = lint_and_fix_python_code(file_path)
 
     if exit_code >= threshold:
         print(f"Linting score ({exit_code}) is above or equal to the threshold ({threshold}).")
