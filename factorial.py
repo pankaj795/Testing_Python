a091991fd7243c697f072add18b3b4bea62aded8
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-06 13:03:52 +0530

    Update factorial.py
    Updated code !!!!

--- factorial.py
+++ factorial.py
@@ -1,10 +1,14 @@
+def add_numbers(a, b):
+    """
+    This function takes two numbers as input and returns their sum.
+    """
+    return a + b
 
-# Python3 program to add two numbers
-num1 = 15
-num2 = 12
- 
-# Adding two nos
-sum = num1 + num2
- 
-# printing values
-print("Sum of", num1, "and", num2 , "is", sum)
+def main():
+    num1 = 10
+    num2 = 20
+    result = add_numbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is: {result}")
+
+if __name__ == "__main__":
+    main()
