cdec1a3847132adacde9dccce0dd2d7b4fe70bff
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-09 14:39:10 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,6 +1,6 @@
 def sum_of_three_numbers(a, b, c,d):
     """
-    Calculate the sum of three numbers
+    Calculate the sum of three numbers.
 
     Args:
         a (int): The first number.
@@ -11,14 +11,18 @@ def sum_of_three_numbers(a, b, c,d):
     Returns:
         int: The sum of the three numbers.
     """
-    return a + b + c +d
+    return a + b + c + d
 
 if __name__ == "__main__":
     num1 = int(input("Enter the first number: "))
     num2 = int(input("Enter the second number: "))
     num3 = int(input("Enter the third number: "))
-    num4 = int(input("Enter the fourth number: "))
+    num1 = int(input("Enter the fourth number: "))
 
-    result = sum_of_three_numbers(num1, num2, num3,num4)
+    result = sum_of_three_numbers(num1, num2, num3,num1)
 
-    print(f"The sum of {num1}, {num2}, {num3} and {num4} is: {result}")
+    print(f"The sum of {num1}, {num2}, {num3} and {num1} is: {result}")
+~                                                                                                                               
+~                                                                                                                               
+~                                                                                                                               
+~                                                             
