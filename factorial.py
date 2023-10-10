3d61dafc890c5aac8a4dbc58e823def68ab55492
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-10 16:08:13 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,22 +1,5 @@
 # This is a simple Python program that follows PEP 8 guidelines for testing.
 
-def add_numbers(a, b):
-    """
-    Add two numbers and return the result.
-
-    Args:
-        a (int): The first number.
-        b (int): The second number.
-
-    Returns:
-        int: The sum of the two numbers.
-
-    Example:
-        >>> add_numbers(2, 3)
-        5
-    """
-    return a + b
-
 def addition_numbers(a, b):
     """
     addition two numbers and return the result.
@@ -34,10 +17,7 @@ def addition_numbers(a, b):
     """
     return a - b
 
-# Test the functions
-if __name__ == "__main__":
-    result1 = add_numbers(2, 7)
-    print(f"Result of adding: {result1}")
+# Test the function
 
     result2 = addition_numbers(10, 2)
     print(f"Result of subtracting: {result2}")
