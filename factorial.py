7efc9c4211e694eca69fe6dcc0f0e3342049ed28
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-10 15:47:35 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -36,8 +36,8 @@ def addition_numbers(a, b):
 
 # Test the functions
 if __name__ == "__main__":
-    result1 = add_numbers(2, 3)
+    result1 = add_numbers(2, 7)
     print(f"Result of adding: {result1}")
 
-    result2 = addition_numbers(5, 2)
+    result2 = addition_numbers(10, 2)
     print(f"Result of subtracting: {result2}")
