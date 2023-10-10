615694e7b6ae72ceb65cd5749576b40df1fc0f3e
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-11 02:08:33 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,9 +1,21 @@
-def sum_of_three_numbers():
-    print("Hello popop")
-   
-   
-    
-    
+'''
+    __G__ = "(G)bd249ce4"
+    logger -> main
+'''
+
+from os import path, environ
+from contextlib import contextmanager
+
+@contextmanager
+def ignore_excpetion(*exceptions):
+    '''
+    catch excpetion
+    '''
+    try:
+        yield
+    except exceptions as error:
+        #print("{} {} {}".format(datetime.utcnow(), EXCLAMATION_MARK, error))
+        pass    
     
    
                                                                                                                                
