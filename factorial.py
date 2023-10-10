cf3b961afd09fa78df13a1150014c93301866f7d
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-11 03:28:17 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,13 +1,16 @@
-# PEP 8 recommends two blank lines before top-level function or class definitions.
+# Sample Python code following PEP 8
 
-def greet(name):
-    # PEP 8 recommends using 4 spaces for indentation (no tabs).
-    """This function greets the person passed in as a parameter."""
-    
-    # PEP 8 recommends using double quotes for docstrings.
+def greeting(name):
+    """This function prints a greeting message."""
     print(f"Hello, {name}!")
 
+def add_numbers(a, b):
+    """This function adds two numbers and returns the result."""
+    return a + b
+
 if __name__ == "__main__":
-    # PEP 8 recommends using "if __name__ == "__main__":" to guard the execution of the script.
-    name = input("Enter your name: ")
-    greet(name)
+    person = "Alice"
+    print("Welcome to our Python program.")
+    greeting(person)
+    result = add_numbers(5, 7)
+    print(f"The sum of 5 and 7 is {result}.")
