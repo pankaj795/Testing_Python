bb95c7c75cd30ae0617ccca6239844660e90810e
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-10 21:46:53 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,5 +1,6 @@
 def sum_of_three_numbers(a, b,c):
     print("Hello popop")
+    print("000000")
     
    
                                                                                                                                
