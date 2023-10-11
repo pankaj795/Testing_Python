2a312ded13074fb13a2097471ea353e7e41e8f41
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-11 15:56:26 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -5,9 +5,9 @@ def greeting(name):
     print(f"Hello, Pankaj!")
  
 
-def add_numbers(a1, b):
+def add_numbers(a1786, b):
     """This function adds two numbers and returns the result."""
-    return a1 + b
+    return a1786 + b
 
 if __name__ == "__main__":
     person = "Alice"
