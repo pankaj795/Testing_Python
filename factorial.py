22620ac0e4d8f873b160f8f882f1febe523f3e89
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-11 16:41:29 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -10,13 +10,15 @@ def calculate_sum(n):
     if n < 1:
         return 0
 
-    total_sum1 = 0
+    total_sum = 0
     for i in range(1, n + 1):
-        total_sum1 += i
+        total_sum += i
 
-    return total_sum1
+    return total_sum
 
 if __name__ == "__main__":
     n = int(input("Enter a positive integer: "))
     result = calculate_sum(n)
     print(f"The sum of numbers from 1 to {n} is: {result}")
+
+~                                      
