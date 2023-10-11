8ab1076f254012cf249a5183d241893135a8894c
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-11 16:22:23 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,15 +1,21 @@
 # Sample Python code following PEP 8
 
 def greeting(name):
-   print("HEELOO PANKAJ")
+    """This function prints a greeting message."""
+    print(f"Hello, {name}!")
 
 def add_numbers(a, b):
     """This function adds two numbers and returns the result."""
     return a + b
- 
+#######################3PANKAJ AGARWAL
 if __name__ == "__main__":
-    person = "Alicehgfjsgdjsgfdsjfhgfgsfgsjgsdjsfgsdjfhgdsjdsfsf"
+    person = "Alice"
     print("Welcome to our Python program.")
     greeting(person)
     result = add_numbers(5, 7)
     print(f"The sum of 5 and 7 is {result}.")
+~                                                                                                                               
+~                                                                                                                               
+~                                                                                                                               
+~                                                                                                                               
+~                                                          
