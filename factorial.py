8759943b13ed6c302c467d196db6f62b326f20ae
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-12 17:04:04 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -10,7 +10,7 @@ def calculate_sum(n):
     return total_sum
 
 if __name__ == "__main__":
-    n = int(input("Enter the password u want to enter:::"))
+    n = int(input("Enter the password u want to entercddeefesdcsdcsdcsc:::"))
     result = calculate_sum(n)
     print(f"The sum of numbers from 1 to {n} is: {result}")
 
