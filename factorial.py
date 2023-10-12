2af81e5a90a3e39122f0406b8a042a3dae43ea29
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-12 12:23:21 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -10,7 +10,7 @@ def calculate_sum(n):
     return total_sum
 
 if __name__ == "__main__":
-    n = int(input("Enter the number U want to print numbersin the seriessdcsdcjnkdsds........... "))
+    n = int(input("Enter the number you want to do::::::::: "))
     result = calculate_sum(n)
     print(f"The sum of numbers from 1 to {n} is: {result}")
 
