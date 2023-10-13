53e24c6a229bb3e6d3778c8182574f1c288e0594
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-13 13:59:14 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -10,7 +10,7 @@ def calculate_sum(n):
     return total_sum
 
 if __name__ == "__main__":
-    n = int(input("Write a positive number:::::::::::::::"))
+    n = int(input("Write a positive number that can be used :::::::::::::::::::::::::::"))
     result = calculate_sum(n)
     print(f"The sum of numbers from 1 to {n} is: {result}")
 
