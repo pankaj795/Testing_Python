52f1b546cccb87a6e12d05d6699e009d8b12f1c5
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-10-13 12:47:12 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -10,7 +10,7 @@ def calculate_sum(n):
     return total_sum
 
 if __name__ == "__main__":
-    n = int(input("Enter the real Positive number u want to create::::::: is saknaknskajsnkasjnksanxkax"))
+    n = int(input("Hii I am thier kenfkfnkfkne"))
     result = calculate_sum(n)
     print(f"The sum of numbers from 1 to {n} is: {result}")
 
