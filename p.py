4b197540b7b7cb667cb38a3292e3d9b6f14e1fe6
pankaj795 <agarwalpankaj20000@gmail.com>
2023-10-30 01:24:01 +0530

    Dddw

--- p.py
+++ p.py
@@ -1,18 +1,6 @@
 # pylint: disable=invalid-name, missing-docstring, unused-argument
 
 
-def w(m):  # Invalid function and argument name
-    a = 146
-    b = 12
-    return a + b
-
-
-# Missing docstring for function
-
-
-class MyClass:
-    def __init__(self):
-        self.var = 1
-
-    def my_method(self):  # Method with no docstring
-        return self.var
+# Invalid function and argument
+a = 146
+b = 12
