ae5a42ca5c7dc163aeb805e020821fb481dfa816
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2023-12-18 17:07:42 +0530

    Update pankaj.py
    dmlnalnal

--- pankaj.py
+++ pankaj.py
@@ -1,17 +1,3 @@
 # pankaj
 
 print("Hello World")
-print("wdwdwdw")
-print("kdjdsflk")
-print("sdddsssssssssssssssssssss")
-print("fssssssssssssssssssss")
-print("dssasdadadadaasd")
-print("tytfferfeffrffe")
-print("ljdskfsnsdbhsdv")
-print("fvdmdflkndflbdv")
-print("Ffsdsfsfsfsf")
-print("fvfvvfdvdv")
-print("ghdfgdhfdgh")
-print("AMITY UNIVERSITY")
-print("fferferferf")
-
